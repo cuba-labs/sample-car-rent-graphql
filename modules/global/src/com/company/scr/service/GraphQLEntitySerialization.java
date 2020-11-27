7bada23f8f118626d6ac660a90e7c8f2a078a288
Vadim Basko <vadim.p.basko@gmail.com>
2020-11-27 15:08:11 +0400

    extend entity serialization bean

---
+++ modules/global/src/com/company/scr/service/GraphQLEntitySerialization.java
@@ -0,0 +1,7 @@
+package com.company.scr.service;
+
+import com.haulmont.cuba.core.app.serialization.EntitySerialization;
+
+public class GraphQLEntitySerialization extends EntitySerialization {
+
+}
